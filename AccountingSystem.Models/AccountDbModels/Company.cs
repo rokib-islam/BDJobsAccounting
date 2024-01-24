15ef644683e191442b0db8082d26e957da56b91b
rokib-islam <rokib.islam00@gmail.com>
2024-01-24 23:51:35 +0600

    Company model update

--- AccountingSystem.Models/AccountDbModels/Company.cs
+++ AccountingSystem.Models/AccountDbModels/Company.cs
@@ -8,26 +8,40 @@ namespace AccountingSystem.Models.AccountDbModels
     public class Company
     {
         public int Id { get; set; }
+        [DisplayName("Company Name")]
         public string Name { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Fax { get; set; }
-        public string Contact_Person { get; set; }
+        public string ContactPerson { get; set; }
         public string Designation { get; set; }
         public double Balance { get; set; }
         public bool BlackListed { get; set; }
-        public int CP_ID { get; set; }
+
+        [DisplayName("Online Id")]
+        [DataType(DataType.Text)]
+        public int CP_Id { get; set; }
+
+        [DisplayName("Acc. Person")]
         public string AccContactName { get; set; }
-        public string VATRegNo { get; set; }
-        public string VATRegAdd { get; set; }
-        public short? DistrictID { get; set; }
-        public int? UpazilaID { get; set; }
-        public short? BankID { get; set; }
+        public string VatRegNo { get; set; }
+        public string VatRegAdd { get; set; }
+
+        public string AccCreatedDate { get; set; }
+
+        public string DistrictId { get; set; }
+
+        public int BankId { get; set; }
+
+        [DisplayName("Vat Challan Name")]
         public string VatChallanName { get; set; }
-        public DateTime? PostedOn { get; set; }
+
+        [DisplayName("Acc. Person Mail")]
         public string AccPersonMail { get; set; }
+
+        [DisplayName("Acc. Person Contact No")]
         public string AccPersonContactNo { get; set; }
     }
 }
