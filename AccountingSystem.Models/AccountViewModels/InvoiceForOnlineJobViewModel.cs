92f39e12a0e4799576e278082f292288381bc962
sujon996691 <sujonmia996691@gmail.com>
2024-02-08 20:38:30 +0600

    online jobs upload invoice

--- AccountingSystem.Models/AccountViewModels/InvoiceForOnlineJobViewModel.cs
+++ AccountingSystem.Models/AccountViewModels/InvoiceForOnlineJobViewModel.cs
@@ -7,12 +7,14 @@
         public string Title { get; set; }
         public double SalesPrice { get; set; }
         public string InvoiceNo { get; set; }
+        public string Invoice_No { get; set; }
         public string Submitted { get; set; }
         public int OpId { get; set; }
         public int AddType { get; set; }
         public int LedgerId { get; set; }
         public string BillingContact { get; set; }
         public double TotalAmount { get; set; }
+        public double TAmount { get; set; }
         public int ServiceID { get; set; }
         public int CompanyID { get; set; }
         public string CompanyName { get; set; }
