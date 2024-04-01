2c280d569c24d1072b19ecd53b4b32c5fd031fe3
Sujon Mia <sujonmia996691@gmail.com>
2024-04-01 16:51:42 +0600

    online payment varification

---
+++ AccountingSystem.Models/AccountViewModels/PaymentVarificationDataModelResponse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AccountingSystem.Models.AccountViewModels
+{
+    public class PaymentVarificationDataModelResponse
+    {
+        public int serialNo { get; set; }
+        public int quotationID { get; set; }
+        public string quotationNo { get; set; }
+        public string uploadDateTime { get; set; }
+        public int companyID { get; set; }
+        public string companyName { get; set; }
+        public string companyAddress { get; set; }
+        public string companyAGM { get; set; }
+        public string serviceName { get; set; }
+        public object jobID { get; set; }
+        public string jobTitle { get; set; }
+        public int totalCost { get; set; }
+        public string billerName { get; set; }
+        public string billerEmail { get; set; }
+        public string billerMobile { get; set; }
+        public string paymentType { get; set; }
+        public string referenceNo { get; set; }
+        public string paymentDocumentLink { get; set; }
+        public int verified { get; set; }
+    }
+
+    public class ApiResponseModel
+    {
+        public string Message { get; set; }
+        public string Error { get; set; }
+        public List<PaymentVarificationDataModelResponse> Data { get; set; }
+        public string pageNo { get; set; }
+        public string pageSize { get; set; }
+        public int totalRecords { get; set; }
+    }
+}
---
+++ AccountingSystem.Models/AccountViewModels/VarifyOrReject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingSystem.Models.AccountViewModels
+{
+    public class VarifyOrReject
+    {
+        public int QID { get; set; }
+        public int verified { get; set; }
+        public int verifiedBy { get; set; }
+    }
+}
