7aafb604c556cda33724ab07325a6250fe16ec19
rokib-islam <rokib.islam00@gmail.com>
2024-05-30 13:31:38 +0600

    modify GetCompanyListByKey

--- AccountingSystem.Repository/CompanyRepository.cs
+++ AccountingSystem.Repository/CompanyRepository.cs
@@ -57,7 +57,7 @@ namespace AccountingSystem.Repository
         {
             using (var _db = new SqlConnection(_DBCon.GetConnectionString("DefaultConnection")))
             {
-                var query = "SELECT TOP 30 Id,Name + ' (' + CONVERT(varchar, Cp_id) + ')' Name, BlackListed, AnyVatExemption FROM Company WHERE Name LIKE @Key ORDER BY Name";
+                var query = "SELECT TOP 30 Id,Name + ' (' + CONVERT(varchar, Cp_id) + ')' Name, BlackListed, AnyVatExemption FROM Company WHERE Name LIKE @Key";
                 var parameters = new { Key = "%" + Key + "%" };
 
                 var result = await _db.QueryAsync<CompanyViewModel>(query, parameters);
