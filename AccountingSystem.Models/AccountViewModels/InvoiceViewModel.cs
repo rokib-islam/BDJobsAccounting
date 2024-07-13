79b002728e3eaec9f8eb890ac8000339cf178e5c
rokib-islam <rokib.islam00@gmail.com>
2024-07-13 11:44:14 +0600

    ok

--- AccountingSystem.Models/AccountViewModels/InvoiceViewModel.cs
+++ AccountingSystem.Models/AccountViewModels/InvoiceViewModel.cs
@@ -3,33 +3,22 @@
     public class InvoiceViewModel
     {
         public int Id { get; set; }
-        public int Cid { get; set; }
-        public string InvoiceNo { get; set; }
-        public string Invoice_No { get; set; }
-        public double TAmount { get; set; }
-        public double TotalAmount { get; set; }
-        public string Comments { get; set; }
-        public string InvSendDt { get; set; }
-        public string Sent { get; set; }
-        public string SendingDt { get; set; }
-        public string SendMode { get; set; }
-        public string FullPayment { get; set; }
-        public string SchAmount { get; set; }
-        public string PostedNature { get; set; }
-        public int Emailed { get; set; }
-        public int TotalPrinted { get; set; }
-        public string Invalid { get; set; }
-        public string UploadedPaymentStatus { get; set; }
-        public string MoneyRecieptNo { get; set; }
+        public string invoice_no { get; set; }
         public string CompanyName { get; set; }
-        public string Label { get; set; }
-        public string CompanyAddress { get; set; }
-        public string SalesPerson { get; set; }
-        public int TotalInvoices { get; set; }
-        public string TaxVatCollectionStatus { get; set; }
-        public string AccPersonMail { get; set; }
+        public decimal tamount { get; set; }
+        public DateTime invsendDt { get; set; }
+        public string Sent { get; set; }
+        public string SendMode { get; set; }
+        public bool Emailed { get; set; }
+        public string FullPayment { get; set; }
+        public int TotalInvoice { get; set; }
         public string SalesPersonName { get; set; }
-        public int CompanyId { get; set; }
+        public string ProductName { get; set; }
+        public string ContactPerson { get; set; }
+        public string ContactPersonPhone { get; set; }
+        public int AgeignDay { get; set; }
+        public int ProductWiseSequence { get; set; }
+        public int LastInvDiff { get; set; }
 
     }
 }
